2f61b8fdc825ec960f932d0f576bebd2c60385c3
AppVeyor <ci@appveyor.com>
2025-03-27 15:23:16 +0300

    add file

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import reactRefresh from "eslint-plugin-react-refresh";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+import typescriptParser from "@typescript-eslint/parser";
+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        es2020: true,
+      },
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": typescriptEslint,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    rules: {
+      // Правила для TypeScript
+    },
+  },
+  {
+    files: ["**/*.jsx", "**/*.js"],
+    rules: {
+      // Правила для JavaScript и React
+    },
+  },
+  {
+    files: ["**/*.css"],
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
+    },
+  },
+];
